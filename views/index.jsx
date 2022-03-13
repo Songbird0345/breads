78001f4ecaef95a66ef42731c47609801eda8219
Amanda Strong <amanda@theheebners.com>
2022-03-13 14:08:21 -0600

    added breads map and names

--- views/index.jsx
+++ views/index.jsx
@@ -4,7 +4,18 @@ const Default = require('./layouts/Default')
 function Index () {
     return (
       <Default>
-        <h2>Index Page</h2>
+{
+  breads.map((bread, index)=> {
+    return (
+      <li key={index}>
+        <a href={`/breads/${index}`}>
+          {bread.name}
+        </a>
+      </li>
+    )
+  })
+}
+
       </Default>
     )
 }
